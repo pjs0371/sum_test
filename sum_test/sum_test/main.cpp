233eb213ccd60b872a45f92bba9fd7c09246e645
jonghan <dlwhdgkssla@naver.com>
2017-01-05 16:38:38 +0900

    Add default files

---
+++ sum_test/sum_test/main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int i,j;
+	cin>>i;
+	j=sum(i);
+	cout<<j;
+	return 0;
+} 
---
+++ sum_test/sum_test/sum.cpp
@@ -0,0 +1,9 @@
+#include"sum.h"
+
+int sum(int n){
+	int i,res;
+	for(i=1;i<=n;i++)
+		res+=i;
+
+	return res;
+}
